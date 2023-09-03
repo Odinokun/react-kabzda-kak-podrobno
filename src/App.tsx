c27bc302c4d82e68bb34a32f3767cacb48a74ce7
odinokun <odinokun@gmail.com>
2023-09-03 15:16:47 +0200

    feat: lesson#3 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,28 @@
-import React from 'react';
 import './App.css';
+import Accordion from './components/Accordion/Accordion';
+import Rating from './components/Rating/Rating';
 
 function App() {
   return (
     <div className='App'>
-      <AppTitle />
-      <Rating />
-      <Accordion />
-      <Rating />
+      <PageTitle title='React kabzda kak podrobno!' />
+      Article 1
+      <Rating value={3} />
+      <Accordion title='Attordion #1' />
+      <Accordion title='Attordion #2' />
+      Article 2
+      <Rating value={0} />
+      <Rating value={1} />
+      <Rating value={2} />
+      <Rating value={3} />
+      <Rating value={4} />
+      <Rating value={5} />
     </div>
   );
 }
 
-function AppTitle() {
-  return <h1>React kabzda kak podrobno</h1>;
-}
-
-function Rating() {
-  return (
-    <>
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-    </>
-  );
-}
-
-function Star() {
-  return <div>star</div>;
-}
-
-function Accordion() {
-  return (
-    <>
-      <AccordionTitle />
-      <AccordionBody />
-    </>
-  );
-}
-
-function AccordionTitle() {
-  return <h3>Menu</h3>;
-}
-
-function AccordionBody() {
-  return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  );
+function PageTitle(props: any) {
+  return <h1>{props.title}</h1>;
 }
 
 export default App;
